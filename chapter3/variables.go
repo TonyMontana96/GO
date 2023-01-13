44eb6bc6543409fff5ec920c64af0c7fca3ed51a
ANton <anton.artiomov@yandex.ru>
2023-01-13 21:17:52 +0400

    Variables

--- chapter3/variables.go
+++ chapter3/variables.go
@@ -1,10 +1,13 @@
 package main
 
-import "fmt"
+import "log"
+
+var x = "Hello World"
 
 func main() {
-	var type x string = "Hello World!"
-	var name type y int8 = 7
-	fmt.Println(x, y)
+	log.Println(x)
+}
 
+func f() {
+	log.Println(x)
 }
